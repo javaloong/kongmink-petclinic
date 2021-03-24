f7cb1c491a00d3dfc5bfbe71b152df458691e4ec
tutunannan <bd66_6@hotmail.com>
2021-03-24 17:00:22 +0800

    fix: retrieve entity manager factory

--- petclinic-app/src/main/java/org/javaloong/kongmink/petclinic/app/Application.java
+++ petclinic-app/src/main/java/org/javaloong/kongmink/petclinic/app/Application.java
@@ -3,10 +3,12 @@ package org.javaloong.kongmink.petclinic.app;
 import org.javaloong.kongmink.pf4j.spring.util.ApplicationContextProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication(proxyBeanMethods = false,
+        exclude = { HibernateJpaAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args) {
